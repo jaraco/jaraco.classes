e61ef724d10f982a9e1d95b71474c12a105070a3
jaraco <none@none>
2005-01-25 15:12:13 +0000

    Added some comments to clarify and removed an unnecessary comma from a singleton list (since it's not a tuple).

--- ClassUtil.py
+++ ClassUtil.py
@@ -1,11 +1,19 @@
+__doc__ = """
+ClassUtil.py provides quick routines for obtaining the class names
+of an object and its parent classes
+"""
 
 import types
 
+# makeSequence puts any non-sequence objects into a singleton list
 def makeSequence(el):
     if type( el ) in ( types.TupleType, types.ListType ):
         return el
-    return [el,]
+    return [el]
 
+# flatten takes a list of lists and returns a single list with each element from the
+#  sublists.  For example,
+#  flatten( [a,b,[c,d,[e,f],g],h] ) == [a,b,c,d,e,f,g,h]
 def flatten( l ):
     if filter( lambda x: type(x) in ( types.ListType, types.TupleType) , l ):
         result = []
@@ -13,8 +21,10 @@ def flatten( l ):
         return tuple( result )
     else: return l
 
+# all bases returns a tuple of all base classes the class c has as a parent.
 def allBases( c ):
     return flatten( tuple(map( allBases, c.__bases__ )) + c.__bases__ )
 
+# all classes returns a tuple of all classes to which c belongs.
 def allClasses( c ):
     return (c,)+allBases(c)
