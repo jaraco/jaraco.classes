898a926991cc1af759791b5a590d58fca450b39f
jaraco <none@none>
2009-06-17 02:58:47 +0000

    Some cleanup of classutil

--- jaraco/util/classutil.py
+++ jaraco/util/classutil.py
@@ -1,9 +1,9 @@
 # -*- coding: UTF-8 -*-
 
-""" ClassUtil.py
-	Provides quick routines for obtaining the class names
+"""ClassUtil.py
+Provides quick routines for obtaining the class names
 of an object and its parent classes.
-	
+
 Copyright © 2004 Jason R. Coombs  
 """
 
@@ -13,27 +13,34 @@ __svnauthor__ = '$Author$'[9:-2]
 __date__ = '$Date$'[7:-2]
 
 import types
+from collections import Sequence
 
-# makeSequence puts any non-sequence objects into a singleton list
-def makeSequence(el):
-    if type(el) in (types.TupleType, types.ListType):
-        return el
-    return [el]
+from jaraco.util.iter_ import flatten
 
-# flatten takes a list of lists and returns a single list with each element from the
-#  sublists.  For example,
-#  flatten([a,b,[c,d,[e,f],g],h]) == [a,b,c,d,e,f,g,h]
-def flatten(l):
-    if filter(lambda x: type(x) in (types.ListType, types.TupleType) , l):
-        result = []
-        map(lambda el, li=result: li.extend(el), map(flatten, map(makeSequence, l)))
-        return tuple(result)
-    else: return l
+def ensure_sequence(el):
+	"""
+	if item is not a sequence, return the item as a singleton in a list
+	>>> ensure_sequence(3)
+	[3]
+	>>> ensure_sequence([3,4])
+	[3, 4]
+	"""
+	if isinstance(el, Sequence):
+		return el
+	return [el]
 
-# all bases returns a tuple of all base classes the class c has as a parent.
-def allBases(c):
-    return flatten(tuple(map(allBases, c.__bases__)) + c.__bases__)
+def all_bases(c):
+	"""
+	return a tuple of all base classes the class c has as a parent.
+	>>> object in all_bases(list)
+	True
+	"""
+	return tuple(flatten(tuple(map(all_bases, c.__bases__)) + c.__bases__))
 
-# all classes returns a tuple of all classes to which c belongs.
-def allClasses(c):
-    return (c,)+allBases(c)
+def all_classes(c):
+	"""
+	return a tuple of all classes to which c belongs
+	>>> list in all_classes(list)
+	True
+	"""
+	return (c,)+all_bases(c)
