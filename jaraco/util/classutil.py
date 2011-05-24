f7a1094ec6e1c6076405baada3d6907c437d0d8f
Jason R. Coombs <jaraco@jaraco.com>
2011-05-24 14:25:06 -0400

    Added itersubclasses to classutil module

--- jaraco/util/classutil.py
+++ jaraco/util/classutil.py
@@ -5,18 +5,13 @@ ClassUtil.py
 Provides quick routines for obtaining the class names
 of an object and its parent classes.
 
-Copyright © 2004, 2009 Jason R. Coombs  
+Copyright © 2004, 2009, 2011 Jason R. Coombs	
 """
 
 __author__ = 'Jason R. Coombs <jaraco@jaraco.com>'
-__version__ = '$Rev$'[6:-2]
-__svnauthor__ = '$Author$'[9:-2]
-__date__ = '$Date$'[7:-2]
 
-import types
 from collections import Sequence
-
-from jaraco.util.iter_ import flatten
+import itertools
 
 def ensure_sequence(el):
 	"""
@@ -45,3 +40,50 @@ def all_classes(c):
 	True
 	"""
 	return c.mro()
+
+# borrowed from http://code.activestate.com/recipes/576949-find-all-subclasses-of-a-given-class/
+def itersubclasses(cls, _seen=None):
+	"""
+	itersubclasses(cls)
+
+	Generator over all subclasses of a given class, in depth-first order.
+
+	>>> list(itersubclasses(int)) == [bool]
+	True
+	>>> class A(object): pass
+	>>> class B(A): pass
+	>>> class C(A): pass
+	>>> class D(B,C): pass
+	>>> class E(D): pass
+	>>> 
+	>>> for cls in itersubclasses(A):
+	...		print(cls.__name__)
+	B
+	D
+	E
+	C
+	>>> # get ALL (new-style) classes currently defined
+	>>> [cls.__name__ for cls in itersubclasses(object)] #doctest: +ELLIPSIS
+	['type', ...'tuple', ...]
+	"""
+	
+	if not isinstance(cls, type):
+		raise TypeError('itersubclasses must be called with '
+			'new-style classes, not %.100r' % cls)
+	if _seen is None: _seen = set()
+	try:
+		subs = cls.__subclasses__()
+	except TypeError: # fails only when cls is type
+		subs = cls.__subclasses__(cls)
+	for sub in subs:
+		if sub in _seen:
+			continue
+		_seen.add(sub)
+		yield sub
+		for sub in itersubclasses(sub, _seen):
+			yield sub
+
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod(verbose=1)
