e276b0c19d43c7cbbfc5ccfc4d7b598652ae364d
Jason R. Coombs <jaraco@jaraco.com>
2019-12-19 13:04:53 -0500

    Rely on more_itertools for unique_everseen

--- jaraco/classes/ancestry.py
+++ jaraco/classes/ancestry.py
@@ -3,7 +3,7 @@ Routines for obtaining the class names
 of an object and its parent classes.
 """
 
-import itertools
+from more_itertools import unique_everseen
 
 
 def all_bases(c):
@@ -66,22 +66,3 @@ def _iter_all_subclasses(cls):
     for sub in subs:
         yield sub
         yield from iter_subclasses(sub)
-
-
-# From Python 3.8 docs
-def unique_everseen(iterable, key=None):
-    "List unique elements, preserving order. Remember all elements ever seen."
-    # unique_everseen('AAAABBBCCDAABBB') --> A B C D
-    # unique_everseen('ABBCcAD', str.lower) --> A B C D
-    seen = set()
-    seen_add = seen.add
-    if key is None:
-        for element in itertools.filterfalse(seen.__contains__, iterable):
-            seen_add(element)
-            yield element
-    else:
-        for element in iterable:
-            k = key(element)
-            if k not in seen:
-                seen_add(k)
-                yield element
