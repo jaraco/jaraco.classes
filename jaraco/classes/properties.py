032ecc1ab358fa50bd8c54c2d1e803bc00d0bbf2
bswck <bswck.dev@gmail.com>
2024-02-07 19:46:00 +0100

    Prefer single quotes

--- jaraco/classes/properties.py
+++ jaraco/classes/properties.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Generic, TypeVar, cast, overload
 
-_T = TypeVar("_T")
+_T = TypeVar('_T')
 
 if TYPE_CHECKING:
     from collections.abc import Callable
@@ -205,7 +205,7 @@ class classproperty(Generic[_T]):
             raise AttributeError("can't set attribute")
         if type(owner) is not classproperty.Meta:
             owner = type(owner)
-        return self.fset.__get__(None, cast("type[object]", owner))(value)
+        return self.fset.__get__(None, cast('type[object]', owner))(value)
 
     def setter(self, fset: _SetterCallable[_T] | _SetterClassMethod[_T]) -> Self:
         self.fset = self._ensure_method(fset)
